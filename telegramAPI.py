fa7875df3cb9395219f29235afb019baef234a52
gogynogy <usernamesgo@gmail.com>
2022-11-08 14:36:07 +0530

    new

---
+++ telegramAPI.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+BOT_TOKEN = "5645949742:AAGDmze2SXINA1kqc4UJjAsSVVNaE9aDjik"
+
+updater = Updater(BOT_TOKEN,
+                  use_context=True)
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        f"Hello sir {update.message.chat.username}."
+    )
+
+def stop_shit(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Stop do this shit"
+    )
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, stop_shit))
+updater.dispatcher.add_handler(MessageHandler(Filters.sticker, stop_shit))
+updater.start_polling()
